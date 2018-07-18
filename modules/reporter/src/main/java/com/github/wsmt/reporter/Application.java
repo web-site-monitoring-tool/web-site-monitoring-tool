f4e321ed0c0fc396004aa49f96076fc3b0003910
Вожаков Александр <a.vozhakov@mail.ru>
2018-07-18 15:30:00 +0600

    write data from hbase into postgres

--- modules/reporter/src/main/java/com/github/wsmt/reporter/Application.java
+++ modules/reporter/src/main/java/com/github/wsmt/reporter/Application.java
@@ -6,13 +6,16 @@ import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructType;
 import scala.Tuple2;
 
 import java.util.Arrays;
+import java.util.Properties;
 
 public class Application {
     public static void main(String[] args) {
@@ -52,9 +55,30 @@ public class Application {
                 )
         );
 
-        applicationConfig.getSqlContext()
-                .createDataFrame(result, schema)
+        Properties connectionProperties = new Properties();
+        connectionProperties.put("user", "wsmt");
+        connectionProperties.put("password", "wsmt");
+
+        Dataset<Row> dataset = applicationConfig.getSqlContext()
+                .createDataFrame(result, schema);
+
+        dataset.show(false);
+
+        dataset.write()
+                .format("jdbc")
+                .option("url", "jdbc:postgresql://postgres:5432/wsmt")
+                .option("dbtable", "statistics.urls")
+                .option("user", "wsmt")
+                .option("password", "wsmt")
+                .mode(SaveMode.Overwrite)
+                .save();
+
+
+//                .write()
+//                .jdbc("jdbc:postgresql://localhost:5432/wsmt",
+//                        "statistics.urls",
+//                        connectionProperties);
+
 //                .registerTempTable("statistics");
-                .show(false);
     }
 }
