862ff08cb84e3570cead60b2b8289889f72910a8
Вожаков Александр <a.vozhakov@mail.ru>
2018-07-18 17:43:37 +0600

    fix api cache

--- modules/api/src/main/java/com/github/wsmt/api/handler/PageStatisticsHandler.java
+++ modules/api/src/main/java/com/github/wsmt/api/handler/PageStatisticsHandler.java
@@ -5,27 +5,24 @@ import com.github.wsmt.api.service.PageStatisticsService;
 import org.springframework.web.reactive.function.server.HandlerFunction;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class PageStatisticsHandler implements HandlerFunction<ServerResponse> {
-    private final Mono<ServerResponse> serverResponse;
+    private final PageStatisticsService pageStatisticsService;
 
-    private PageStatisticsHandler(Mono<ServerResponse> serverResponse) {
-        this.serverResponse = serverResponse;
+    private PageStatisticsHandler(PageStatisticsService pageStatisticsService) {
+        this.pageStatisticsService = pageStatisticsService;
     }
 
     public static PageStatisticsHandler byPageService(PageStatisticsService pageStatisticsService) {
-        return new PageStatisticsHandler(
-                ServerResponse.ok()
-                        .body(
-                                pageStatisticsService.pageStatistics(),
-                                PageStatistics.class
-                        )
-        );
+        return new PageStatisticsHandler(pageStatisticsService);
     }
 
     @Override
     public Mono<ServerResponse> handle(ServerRequest request) {
-        return serverResponse;
+        Flux<PageStatistics> pageStatistics = pageStatisticsService.pageStatistics();
+        return ServerResponse.ok()
+                .body(pageStatistics, PageStatistics.class);
     }
 }
