b9a56451de871d11fd04366cc58f02a54ba28237
Вожаков Александр <a.vozhakov@mail.ru>
2018-08-01 14:11:29 +0600

    create tests for GeoIpCountryDb in reporter

---
+++ modules/pixel-tracker/src/test/java/com/github/wsmt/tracker/handler/PixelHandlerTest.java
@@ -0,0 +1,48 @@
+package com.github.wsmt.tracker.handler;
+
+import com.github.wsmt.tracker.service.UserService;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpCookie;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.mockito.Mockito.*;
+
+import static org.junit.Assert.*;
+
+public class PixelHandlerTest {
+
+    @Test
+    public void testCreation() {
+        UserService userService = mock(UserService.class);
+        Resource resource = mock(Resource.class);
+        String profileIdCookieName = "123456";
+        PixelHandler pixelHandler = PixelHandler.newPixelHandler(resource, profileIdCookieName, userService);
+
+        assertNotNull(pixelHandler);
+    }
+
+    @Ignore
+    @Test
+    public void testGetUser() {
+        UserService userService = mock(UserService.class);
+        Resource resource = mock(Resource.class);
+        ServerRequest serverRequest = mock(ServerRequest.class);
+
+        String cookieName = "cookie";
+        String cookieValue = "123456";
+
+        when(serverRequest.cookies()
+                .getFirst(cookieName))
+                .thenReturn(new HttpCookie(cookieName, cookieValue));
+
+        PixelHandler pixelHandler = PixelHandler.newPixelHandler(resource, cookieName, userService);
+        Mono<ServerResponse> responseMono = pixelHandler.handle(serverRequest);
+
+        assertTrue(resource.exists());
+    }
+
+}
---
+++ modules/reporter/src/main/java/com/github/wsmt/reporter/db/impl/GeoIpCountryTest.java
@@ -0,0 +1,28 @@
+package com.github.wsmt.reporter.db.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GeoIpCountryTest {
+
+    @Test
+    public void byDbName() {
+        GeoIpCountry geoIpCountry = GeoIpCountry.byDbName("GeoLite2-City.mmdb");
+        assertNotNull(geoIpCountry);
+    }
+
+    @Test
+    public void getLocation() {
+        GeoIpCountry geoIpCountry = GeoIpCountry.byDbName("GeoLite2-City.mmdb");
+        String country = geoIpCountry.getLocation("94.251.95.50");
+        assertEquals("Russia", country);
+    }
+
+    @Test
+    public void getLocationWithNotCorrectIp() {
+        GeoIpCountry geoIpCountry = GeoIpCountry.byDbName("GeoLite2-City.mmdb");
+        String country = geoIpCountry.getLocation("94.251.95eee");
+        assertEquals("Unknown", country);
+    }
+}
